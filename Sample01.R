9f8b82fc96d06c9f560083ac76c77fa8aec9cbd8
ingini <50002919+ingini@users.noreply.github.com>
2019-06-28 17:12:26 +0900

    Add files via upload

--- Sample01.R
+++ Sample01.R
@@ -1,100 +1,70 @@
-df <- read.csv("income.csv", 
-               header = TRUE,
-               stringsAsFactors = FALSE)
-str(df)
-# plyr(x)
-# dplyr(o)
-options(repos = c(CRAN = 'https://cloud.r-project.org'))
-install.packages('dplyr', repos = 'https://cloud.r-project.org')
-#library(dplyr)
+ls("package:datasets")
+test_data <-
+  c(850,740,900,1050,1020,940,930,870,980,900,
+    800,740,630,1050,960,960,810,760,980,1000)
+test_data
+plot(test_data)
+plot(1:10)
+stem(test_data)
+hist(test_data)
+qqnorm(test_data)
+boxplot(test_data)
+summary(test_data)
+mean(test_data)
+min(test_data)
+sd(test_data)
+var(test_data)
 
-df
-sample_n(df,3)
-sample_frac(df, 0.1)
-df2 <- distinct(df)
-df2
-df <- read.csv("income.csv", 
-               header = TRUE,
-               stringsAsFactors = TRUE)
-str(df)
-# select
-select(df,Index,State:Y2008)
-select(df,Index,State)
-select(df, State)
-select(df, -Index, -State)
-select(df, -c(Index,State))
-select(df, starts_with("Y"))
-select(df, -starts_with("Y"))
-select(df, contains("I"))
-select(df, State, everything())
-df2 <- rename(df, Index2=Index)
-names(df2)
+plot(attitude)
 
-# filter
-filter(df, Index == "A")
-filter(df, Index %in% "A")
-filter(df, Index %in% c("A","C"))
-filter(df, Index %in% c("A", "C") & Y2002 >= 1400000)
-filter(df, !Index %in% c("A","C"))
-# 010-1234-1234 -> [0-9][0-9][0-9] => 정규표현식
-# regular expression
-filter(df, grepl("Ar", State))
+# y = 2x
+# x -> y
 
-summarise(df, Y2015_mean = mean(Y2015),
-          Y2015_median = median(Y2015))
+# y x -> y = ()x + () -> y ~ x
+# 2 1
+# 3 2
+# y = ()x2 + ()x + ()
+# y ~ x + y
+test <- lm(rating ~ complaints, data=attitude)
+test
+summary(test)
 
-# arrange
-df3 <- select(df, Index)
-df3
-df4 <- arrange(df3, Index)
-head(df4)
-df5 <- arrange(df3, desc(Index))
-df5
+# Microsoft R DataSet
+library(readr)
+library(RevoScaleR)
+list.files(rxGetOption("sampleDataDir"))
+inDataFile <- 
+  file.path(rxGetOption("sampleDataDir"),
+            "mortDefaultSmall2000.csv")
+mortData <- rxImport(inData = inDataFile)
+str(mortData)
+rxGetVarInfo(mortData)
+nrow(mortData)
+ncol(mortData)
+names(mortData)
+head(mortData, 3)
+rxGetInfo(mortData, getVarInfo = TRUE, numRows = 5)
+rxHistogram(~creditScore, data=mortData)
+mortData2 <-
+  rxDataStep(inData = mortData,
+             varsToDrop = c("year"),
+             rowSelection = creditScore < 800)
+rxHistogram(~creditScore, data=mortData2)
 
-df6 <- select(df, Index, State)
-df6
-df7 <- arrange(df6, desc(Index), State)
-df7
-#1 1
-#1 2
-#1 1
-#1 2
-#2 1
-#3 2
+mortData3 <- rxDataStep(
+  inData = mortData, 
+  varsToDrop = c("year"),
+  rowSelection = creditScore < 800, 
+  transforms = list(catDept = cut(ccDebt, 
+              breaks = c(0,6500,13000),
+              labels = c("Low Debt", "High Debt")),
+  lowScore = creditScore < 625))
 
-# group_by
-group_by(df, Index)
+rxGetVarInfo(mortData3)
 
-View(df)
-head(df,5)
-tail(df,5)
-View(df)
+mortCube <- rxCube(~F(creditScore):catDept, 
+                   data = mortData3)
 
-# Pipe Operator %>%
-# magrittr
-df %>% select(Index, State)
-
-sample_n(select(df, Index,State), 10)
-df %>% select(Index,State) %>% sample_n(10)
-
-df10 <- read.csv("exam.csv")
-str(df10)
-df10
-filter(df10, class == 1)
-df10 %>% filter(class == 1)
-
-filter(select(df10, class, math, english), class == 1)
-df10 %>% 
-  select(class, math, english) %>%
-  filter(class == 1) %>%
-  arrange(desc(math))
-
-df_temp <- df10 %>%
-  filter(class %in% c(1,3,5)) %>%
-  arrange(desc(english))
-
-df_temp
-
-
-
-
+head(mortCube)
+rxLinePlot(Counts ~ creditScore|catDept,
+           data=rxResultsDF(mortCube))
